ad42734dd4fdc23777f3cdbe673d01c35fd37421
Manuel Tobío Aldao <teymanu@gmail.com>
2021-08-07 00:05:44 +0200

    use hash
    use hash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { RecipeEffects } from './recipes/store/recipe.effects';
-import { environment } from './../environments/environment.prod';
-import { AuthEffects } from './auth/store/auth.effects';
-import { appReducer } from './store/app.reducer';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { EffectsModule } from '@ngrx/effects';
 
+import { environment } from './../environments/environment.prod';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
@@ -13,19 +15,18 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { HeaderComponent } from './header/header.component';
 
+import { RecipeEffects } from './recipes/store/recipe.effects';
+import { AuthEffects } from './auth/store/auth.effects';
 import { RecipesRoutingModule } from './recipes/recipes-routing.module';
 import { CoreModule } from './core.module';
-import { StoreModule } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     RecipesRoutingModule,
     SharedModule,
