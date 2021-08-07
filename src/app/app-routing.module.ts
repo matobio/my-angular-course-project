8948a5fafae452adc5cab97fbcd9df812a950c91
Manuel Tobío Aldao <teymanu@gmail.com>
2021-08-07 13:26:10 +0200

    fix 404
    fix 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: '**', redirectTo: '/recipes' },
   {
     path: 'recipes',
     loadChildren: () =>
