6dda912deb0424c966ef066540efe4d23d64cb11
Manuel Tobío Aldao <teymanu@gmail.com>
2021-08-07 13:15:00 +0200

    fix redirect on github pages
    fix redirect on github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
+  imports: [RouterModule.forRoot(appRoutes, { useHash: false })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
